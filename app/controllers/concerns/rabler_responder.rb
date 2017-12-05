66f01703504ee1f6c5504542bcc150e591c6f05f
Juanmanuel Berrocal <juanmaberrocal37@gmail.com>
2017-12-05 14:49:45 -0600

    set auth headers on rabl `error!` responses
    Signed-off-by: Juanmanuel Berrocal <juanmaberrocal37@gmail.com>

--- app/controllers/concerns/rabler_responder.rb
+++ app/controllers/concerns/rabler_responder.rb
@@ -1,22 +1,32 @@
+def auth_headers(headers)
+  {}.tap do |auth_header|
+    auth_header['access-token'] = headers['Access-Token']
+    auth_header['client'] = headers['Client']
+    auth_header['expiry'] = headers['Expiry']
+    auth_header['token'] = headers['Token']
+    auth_header['uid'] = headers['Uid']
+  end
+end
+
 module RablerResponder
   extend ActiveSupport::Concern
 
   included do
     helpers do
       def bad_request(error = 'Bad Request')
-        error!({ error: error }, 400)
+        error!({ error: error }, 400, auth_headers(headers))
       end
 
       def unauthorized(error = 'Unauthorized')
-        error!({ error: error }, 401)
+        error!({ error: error }, 401, auth_headers(headers))
       end
 
       def forbidden(error = 'Forbidden')
-        error!({ error: error }, 403)
+        error!({ error: error }, 403, auth_headers(headers))
       end
 
       def not_found(error = 'Not Found')
-        error!({ error: error }, 404)
+        error!({ error: error }, 404, auth_headers(headers))
       end
 
       def unprocessable_entity(model)
@@ -24,7 +34,7 @@ module RablerResponder
         model.errors.keys.each do |error_key|
           error[error_key] = model.errors[error_key].first
         end
-        error!({ error: error }, 422)
+        error!({ error: error }, 422, auth_headers(headers))
       end
 
       def accepted
