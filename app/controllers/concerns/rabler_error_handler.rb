c81d612deb5608e24b55602a9a1924497e717b99
Juanmanuel Berrocal <juanmaberrocal37@gmail.com>
2017-12-06 20:25:48 -0600

    handle error response with auth headers
    Signed-off-by: Juanmanuel Berrocal <juanmaberrocal37@gmail.com>

--- app/controllers/concerns/rabler_error_handler.rb
+++ app/controllers/concerns/rabler_error_handler.rb
@@ -1,5 +1,17 @@
+# description: build raw rack error response
+#              to be used if dependent helper methods not availble
+# @error: error string
+# @code: error code
+# @return: rack error
+def rack_response(error, code = 500)
+  Rack::Response.new({
+    error: error
+  }.to_json, code).finish
+end
+
 # concern module to rescue errors in grape api
 # - build rack errors in standard format
+# - dependent on RablerResponder for helper return methods
 module RablerErrorHandler
   extend ActiveSupport::Concern
 
@@ -12,23 +24,30 @@ module RablerErrorHandler
         # keep the response simple for now as string.
         error[error_key.first] = e.errors[error_key].join(',')
       end
-      Rack::Response.new({
-        error: error
-      }.to_json, 400)
+
+      begin
+        bad_request(error)
+      rescue
+        rack_response(error, 400)
+      end
     end
 
     # 404 Eror - Not Found
     rescue_from ActiveRecord::RecordNotFound do |e|
-      Rack::Response.new({
-        error: e.message
-      }.to_json, 404)
+      begin
+        not_found(e.message)
+      rescue
+        rack_response(e.message, 404)
+      end
     end
 
     # 500 Error - *
-    # rescue_from :all do |e|
-    #   Rack::Response.new({
-    #     error: e.message
-    #   }.to_json, 500).finish
-    # end
+    rescue_from :all do |e|
+      begin
+        server_error(e.message)
+      rescue
+        rack_response(e.message)
+      end
+    end
   end
 end
--- app/controllers/concerns/rabler_responder.rb
+++ app/controllers/concerns/rabler_responder.rb
@@ -1,8 +1,9 @@
 # description: build authorization headers hash based on
 #              original headers that were going to be returned
-# @headers: original grape headers
 # @return: headers hash
-def auth_headers(headers)
+def auth_headers
+  headers = env['api.endpoint'].headers
+
   {}.tap do |auth_header|
     auth_header['access-token'] = headers['Access-Token']
     auth_header['client'] = headers['Client']
@@ -22,19 +23,19 @@ module RablerResponder
   included do
     helpers do
       def bad_request(error = 'Bad Request')
-        error!({ error: error }, 400, auth_headers(headers))
+        error!({ error: error }, 400, auth_headers)
       end
 
       def unauthorized(error = 'Unauthorized')
-        error!({ error: error }, 401, auth_headers(headers))
+        error!({ error: error }, 401, auth_headers)
       end
 
       def forbidden(error = 'Forbidden')
-        error!({ error: error }, 403, auth_headers(headers))
+        error!({ error: error }, 403, auth_headers)
       end
 
       def not_found(error = 'Not Found')
-        error!({ error: error }, 404, auth_headers(headers))
+        error!({ error: error }, 404, auth_headers)
       end
 
       def unprocessable_entity(model)
@@ -42,7 +43,11 @@ module RablerResponder
         model.errors.keys.each do |error_key|
           error[error_key] = model.errors[error_key].first
         end
-        error!({ error: error }, 422, auth_headers(headers))
+        error!({ error: error }, 422, auth_headers)
+      end
+
+      def server_error(error = 'Internal Server Error')
+        error!({ error: error }, 500, auth_headers)
       end
 
       def accepted
